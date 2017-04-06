eedaa9613afa2a19ea27e4f5a742a1217e4c21b5
tjabbe <martin.goutry@gmail.com>
2017-04-06 17:07:07 +0200

    Add Header styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,15 @@
 import React, { PropTypes } from 'react'
+import { prefixLink } from 'gatsby-helpers'
 import CloseButton from '../CloseButton'
+import styles from './header.module.scss'
 
 const Header = ({ showCloseButton }) => (
-  <header>
-    <p>Test</p>
-    {showCloseButton && <CloseButton />}
+  <header className={styles.root}>
+    <div>
+      <h2 className={`${styles.text} ${styles.name}`}>Amine Bouneggar</h2>
+      <h3 className={`${styles.text} ${styles.role}`}>French designer</h3>
+    </div>
+    {showCloseButton && <CloseButton href={prefixLink('/')} />}
   </header>
 )
 
