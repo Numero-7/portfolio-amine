f44d13602e500a54533d6f872498732667eaa3b9
Tom Bonnike <bonniketom@gmail.com>
2017-04-13 10:43:37 +0200

    Sorted project data by their order

--- src/utils/get-projects-data.js
+++ src/utils/get-projects-data.js
@@ -4,6 +4,7 @@ const getProjectsData = pages => (
   pages
     .filter(page => isProjectPage(page))
     .map(page => Object.assign(page.data, { path: page.path }))
+    .sort((a, b) => a.order - b.order)
 )
 
 export default getProjectsData
