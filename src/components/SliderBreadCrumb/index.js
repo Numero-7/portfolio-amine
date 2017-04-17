a5f2dad0b57e445e4ba6fa360d92870bd8b4a82a
tjabbe <martin.goutry@gmail.com>
2017-04-17 16:47:16 +0200

    Add SliderBreadCrumb Component

---
+++ src/components/SliderBreadCrumb/index.js
@@ -0,0 +1,35 @@
+import React, { PropTypes } from 'react'
+import styles from './slider-breadcrumb.module.scss'
+
+const SliderBreadCrumb = ({ projectsData, handleProjectSwitch, currentIndex }) => (
+  <div className={styles.root}>
+
+    <ul className={styles.list}>
+      {projectsData.map((project, index) => (
+        <li className={styles.item}>
+          <div>
+            <button
+              className={`${styles.number} ${currentIndex === index ? styles.active : ''}`}
+              onClick={() => handleProjectSwitch(index)}
+            >
+
+              {currentIndex === index
+                ? project.title
+                : `0${project.order}`}
+
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+
+  </div>
+)
+
+SliderBreadCrumb.propTypes = {
+  projectsData: PropTypes.arrayOf(PropTypes.object).isRequired,
+  handleProjectSwitch: PropTypes.func.isRequired,
+  currentIndex: PropTypes.number.isRequired
+}
+
+export default SliderBreadCrumb
