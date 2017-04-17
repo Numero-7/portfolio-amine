7d0f0d8b3f60f6b81b219cdd9395ed3b71bfdbe5
tjabbe <martin.goutry@gmail.com>
2017-04-17 16:46:43 +0200

    Add Slider Component

---
+++ src/components/Slider/index.js
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react'
+import SliderCover from '../SliderCover'
+import styles from './slider.module.scss'
+
+class Slider extends Component {
+
+  render () {
+    return (
+      <section>
+        <SliderCover />
+      </section>
+    )
+  }
+}
+
+export default Slider
