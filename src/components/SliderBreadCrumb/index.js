ae0a7e4e93780968a3645747407a617cd6f0bad9
tjabbe <martin.goutry@gmail.com>
2017-05-02 17:46:34 +0200

    Lint js/scss

--- src/components/SliderBreadCrumb/index.js
+++ src/components/SliderBreadCrumb/index.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import styles from './slider-breadcrumb.module.scss'
 
 const getLabel = (currentIndex, index, title, order) => {
@@ -11,38 +11,44 @@ const getLabel = (currentIndex, index, title, order) => {
   return label
 }
 
-const SliderBreadCrumb = ({ projectsData, handleProjectSwitch, currentIndex }) => (
-  <div className={styles.root}>
-    <ul className={styles.list}>
-      {projectsData.map((project, index) => (
-        <li
-          key={project.shortTitle}
-          className={`${styles.item} ${currentIndex === index ? styles.active : ''}`}
-        >
-          <div>
-            <button
-              className={`${styles.number} ${currentIndex === index ? styles.active : ''}`}
-              onClick={() => handleProjectSwitch(index)}
+class SliderBreadCrumb extends Component {
+  static propTypes = {
+    projectsData: PropTypes.arrayOf(PropTypes.object).isRequired,
+    handleProjectSwitch: PropTypes.func.isRequired,
+    currentIndex: PropTypes.number.isRequired
+  }
+
+  render () {
+    const { projectsData, handleProjectSwitch, currentIndex } = this.props
+
+    return (
+      <div className={styles.root}>
+        <ul className={styles.list}>
+          {projectsData.map((project, index) => (
+            <li
+              key={project.shortTitle}
+              className={`${styles.item} ${currentIndex === index ? styles.active : ''}`}
             >
-              {getLabel(currentIndex, index, project.title, project.order)}
-            </button>
+              <div>
+                <button
+                  className={`${styles.number} ${currentIndex === index ? styles.active : ''}`}
+                  onClick={() => handleProjectSwitch(index)}
+                >
+                  {getLabel(currentIndex, index, project.title, project.order)}
+                </button>
 
-            <div
-              ref={(component) => { this.thumbnail = component }}
-              className={`${styles.projectThumbnail} ${currentIndex === index ? styles.activeThumbnail : ''}`}
-              style={{ backgroundImage: `url(${project.cover})` }}
-            />
-          </div>
-        </li>
-      ))}
-    </ul>
-  </div>
-)
-
-SliderBreadCrumb.propTypes = {
-  projectsData: PropTypes.arrayOf(PropTypes.object).isRequired,
-  handleProjectSwitch: PropTypes.func.isRequired,
-  currentIndex: PropTypes.number.isRequired
+                <div
+                  ref={(component) => { this.thumbnail = component }}
+                  className={`${styles.projectThumbnail} ${currentIndex === index ? styles.activeThumbnail : ''}`}
+                  style={{ backgroundImage: `url(${project.cover})` }}
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default SliderBreadCrumb
