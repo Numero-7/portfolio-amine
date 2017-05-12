d4c1cfb994881409820db0af5becba346debb7da
Tom Bonnike <bonniketom@gmail.com>
2017-05-12 14:24:33 +0200

    Added more Smoke performance optimizations

--- src/components/Smoke/index.js
+++ src/components/Smoke/index.js
@@ -19,13 +19,35 @@ class Smoke extends Component {
     this.scene = new Scene()
     this.base.appendChild(this.scene.renderer.view)
     this.emitter = new Emitter(this.scene)
+  }
 
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.opacity && !this.tickerStarted) {
+      // By default, the opacity is set to 0. If we start having an opacity superior to 0, it means
+      // that we should start animating the smoke.
+      this.startTicker()
+    } else {
+      // The opacity will be reset to 0 only before switching project in the slider. If this is the
+      // case, we should pause the ticker for performance reasons.
+      this.removeTicker()
+    }
+  }
+
+  componentWillUnmount () {
+    this.removeTicker()
+  }
+
+  startTicker () {
+    this.tickerStarted = true
     this.tickListener = () => this.update()
     TweenLite.ticker.addEventListener('tick', this.tickListener)
   }
 
-  componentWillUnmount () {
-    TweenLite.ticker.removeEventListener('tick', this.tickListener)
+  removeTicker () {
+    if (this.tickerStarted) {
+      this.tickerStarted = false
+      TweenLite.ticker.removeEventListener('tick', this.tickListener)
+    }
   }
 
   update () {
